6ce40dcd02b1958bb9a9bd616616c0cf987457c4
Tim Seryakov <t.seryakov@gmail.com>
2021-01-29 16:04:15 +0300

    pre install dnd lib

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -153,12 +153,10 @@ const DeleteIcon = () => {
     )
 }
 
-
 const DrugHandlerIcon = () => {
     return (
         <svg className="w-6 h-6 mt-2 mr-1 text-gb-text opacity-40" fill="currentColor">
-            <path
-                  d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2
+            <path d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2
                      2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2
                      2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"
             />
